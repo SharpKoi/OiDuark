ec6ae9f845fe94856c32228f1bc02059c2e3ab8c
SharpKoi <30861942+SharpKoi@users.noreply.github.com>
2022-03-26 23:42:23 +0800

    style: rename package 'fxml' to 'view'

---
+++ src/com/sharpkoi/oiduark/app/view/sample/DraggableListCell.java
@@ -0,0 +1,62 @@
+package com.sharpkoi.oiduark.app.view.sample;
+
+import com.sharpkoi.oiduark.utils.Console;
+
+import javafx.scene.Cursor;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.input.MouseEvent;
+
+public class DraggableListCell<T> extends ListCell<T> {
+	
+	private boolean dragging;
+	
+	private double X1;
+	private double Y1;
+	
+	@Override
+	protected void updateItem(T item, boolean empty) {
+		super.updateItem(item, empty);
+		
+		if(item == null || empty) {
+			setGraphic(null);
+			setText(null);
+			return;
+		}
+		
+		Label l = new Label(item.toString());
+		setGraphic(l);
+		
+		setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+		setCursor(Cursor.OPEN_HAND);
+		
+		setOnMousePressed(e -> {
+			if(!empty) {
+				setCursor(Cursor.CLOSED_HAND);
+				Console.INFO("x: %f, y: %f", getLayoutX(), getLayoutY());
+				setDragging(true, e);
+			}
+		});
+		
+		setOnMouseDragged(e -> {
+			if(!empty) {
+				
+			}
+		});
+		
+		setOnMouseReleased(e -> {
+			if(!empty) {
+				setCursor(Cursor.OPEN_HAND);
+			}
+		});
+	}
+	
+	protected void setDragging(boolean b, MouseEvent e) {
+		dragging = b;
+		if(dragging) {
+			X1 = e.getScreenX();
+			Y1 = e.getScreenY();
+		}
+	}
+}
